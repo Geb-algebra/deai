1bcc42f5813783d75fae302a9e1a9a71f58aab6d
Geb-algebra <77182026+Geb-algebra@users.noreply.github.com>
2025-06-22 21:09:27 +0900

    fix importing built stuff

--- worker.ts
+++ worker.ts
@@ -3,6 +3,6 @@ import handle from "hono-react-router-adapter/cloudflare-workers";
 import server from "./app/server";
 
 // @ts-ignore
-import * as build from "./build/server";
+import * as build from "virtual:react-router/server-build";
 
 export default handle(build, server);
